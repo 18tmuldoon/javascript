4a8383b80164a614140651f8828191ea522b7c88
18tmuldoon <112865269+18tmuldoon@users.noreply.github.com>
2022-10-07 12:09:52 +0100

    Add files via upload

---
+++ exercise3.js
@@ -0,0 +1,48 @@
+//1
+console.log("* = multiplication operator");
+console.log("'hello' = string");
+console.log("-88.8 = Number")
+console.log("- = subtration operator")
+console.log("/ = division operator")
+console.log("+ = addition operator")
+console.log("5 = number")
+
+//2
+console.log("spam = variable")
+console.log("'spam' = string")
+
+//3
+console.log("Numbers, Strings and Variables");
+
+//4
+console.log("21")
+
+//5
+console.log("spamspamspam")
+console.log("NaN")
+
+//6
+let input = String(prompt("Input 1 , 2 or anything"));
+if(input == 1){
+    console.log("Hello")
+}else if(input == 2){
+    console.log("Howdy")
+}else{
+    console.log("Greetings!")
+}
+
+//7
+console.log("1 = The capital C")
+console.log("2 = The capital CONSOLE.LOG")
+console.log("3 = The capital L")
+console.log("4 = The LACK OF . ")
+
+//number8
+console.log("This will not print anything");
+console.log("This will print the value for the varianle 'Hello World!'");
+console.log("This will not print anything");
+console.log("This will print 'Hello World1'");
+console.log("This will hide 'console.log(Hello World!)");
+console.log("This will return as an error");
+console.log("This will print 'Hello World!' and then 'Welcome to JavaScript' underneath it");
+console.log("This is will hide all the elements of code as a comment");
