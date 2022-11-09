bbceae839810fb23843100c21e6313893318b31e
18tmuldoon <112865269+18tmuldoon@users.noreply.github.com>
2022-11-09 10:58:49 +0000

    Add files via upload

---
+++ ex10.js
@@ -0,0 +1,9 @@
+let row = Number(prompt("enter amount of rows"));
+let col = Number(prompt("enter amount of columns"));
+
+for(let j= 0; j<row; j++){
+    for (let k = 0; k<col; k++){
+    document.write("*" );
+    }
+    document.write("<br />");
+}
