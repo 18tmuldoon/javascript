6bb533bc87566fa8afa8ef20f7b3abc65279c3f6
18tmuldoon <112865269+18tmuldoon@users.noreply.github.com>
2022-11-10 11:55:49 +0000

    Add files via upload

--- ex10.js
+++ ex10.js
@@ -1,9 +1,34 @@
+/*
+//1
+for(let j= 0; j<5; j++){
+    for (let k = 0; k<5; k++){
+    document.write("*" );
+    }
+    document.write("<br />");
+}
+
+//2
 let row = Number(prompt("enter amount of rows"));
 let col = Number(prompt("enter amount of columns"));
 
-for(let j= 0; j<row; j++){
-    for (let k = 0; k<col; k++){
-    document.write("*" );
+for(let j= 0; j<col; j++){
+    for (let k = 0; k<row; k++){
+    document.write(" * ");
+    }
+    document.write("<br />");
+}
+*/
+
+//3
+let row1 = Number(prompt("enter amount of rows"));
+let col1 = Number(prompt("enter amount of columns"));
+
+for(let j= 0; j<col1; j++){
+    for (let k = 0; k<row1; k++){
+    document.write(" * ");
+    if(j = 1 && k > 1){
+        document.write("  "); 
+    }
     }
     document.write("<br />");
 }
