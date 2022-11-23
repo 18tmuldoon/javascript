1485277be731eb1fc0bbac9fa7ceec8b08f01bc9
18tmuldoon <112865269+18tmuldoon@users.noreply.github.com>
2022-11-23 10:45:20 +0000

    Add files via upload

--- ex11.js
+++ ex11.js
@@ -47,10 +47,66 @@ document.write("The Min digit is:");
 document.write("<br>");
 document.write(minValue);
 
+document.write("<br>");
+
 //6
+let MyArrayCopy = MyArray
+let maxValue1 = Math.max(...MyArrayCopy);
+let n1=1;
+while(n1<=10){
+    n1++;
+if(MyArray[n1]==maxValue1){
+    MyArrayCopy[n1] = 0
+}
+}
+let maxValue2 = Math.max(...MyArrayCopy);
+document.write("The Second largest digit is:");
+document.write("<br>");
+document.write(maxValue2);
+
+//7
+    document.write("<br>");
+let num1=1;
+let even=0;
+let odd=0;
+while(num1<=10){
+    num1++;
+if((MyArray[num1]%2)==0){
+    even++
+}else{
+    odd++
+}
+}
+document.write("Total of even is ");
+document.write(even);
+document.write("<br>")
+document.write("Total of odd is ");
+document.write(odd);
+
+document.write("<br>")
+
+//8
+let MyArrayCopy1 = MyArray
+document.write("Copy array = ")
+document.write(MyArrayCopy1)
+
+document.write("<br>")
+
+//9
+
+document.write("Add a Digit")
+document.write("<br>")
+MyArrayCopy1.push(8)
+
+document.write(MyArrayCopy1)
+
+document.write("<br>")
 
 
+//10
+document.write("Remove a Digit")
+document.write("<br>")
+MyArrayCopy1.pop()
 
-
-
+document.write(MyArrayCopy1)
 document.write("<br/ >");
