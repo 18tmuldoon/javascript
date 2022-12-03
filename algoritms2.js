81f75a4a582bd75b85b941fc718845d81a3464bb
18tmuldoon <112865269+18tmuldoon@users.noreply.github.com>
2022-12-03 00:06:56 +0000

    Add files via upload

---
+++ algoritms2.js
@@ -0,0 +1,7 @@
+character = prompt("Enter character");
+
+if(character == "a"|character == "e"|character == "i"|character == "o"|character == "u"){
+    document.write("Your character IS a vowel");
+}else{
+    document.write("Your character is NOT a vowel")
+}
